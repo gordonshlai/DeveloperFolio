ae8ce3ea5a2b8baea4fe1be726584ff3459dd3da
gordonshlai <gordonshlai@yahoo.com.hk>
2022-03-02 18:30:56 +0000

    Added splashScreen

--- src/portfolio.js
+++ src/portfolio.js
@@ -4,6 +4,16 @@
 
 import emoji from "react-easy-emoji";
 
+import splashAnimation from "./assets/lottie/splashAnimation"; // Rename to your file name for custom animation
+
+// Splash Screen
+
+const splashScreen = {
+  enabled: true, // set false to disable splash screen
+  animation: splashAnimation,
+  duration: 2000 // Set animation duration as per your animation
+};
+
 // Summary And Greeting Section
 
 const illustration = {
@@ -382,6 +392,7 @@ export {
   illustration,
   greeting,
   socialMediaLinks,
+  splashScreen,
   skillsSection,
   educationInfo,
   techStack,
